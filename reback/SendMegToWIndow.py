1e10f96e28761f4fe584a44635e698283f367e4f
tron3 <1257212188@qq.com>
2022-01-17 13:23:20 +0800

    optimized the sendmsg

--- reback/SendMegToWIndow.py
+++ reback/SendMegToWIndow.py
@@ -2,22 +2,26 @@ import win32gui
 import win32con
 import win32clipboard as w
 import time
-lst = ["hello","world","dont","let","me","down"]
 #发送的消息
-
+lst = ["这","是","个","测","代码","吗"]
+#发送时间间隔
+flashTi = int(5)
 #窗口名字
 name = "你"
-#将测试消息复制到剪切板中
-
 #获取窗口句柄
 handle = win32gui.FindWindow(None, name)
-while 1==1:
+#循环次数
+loopTis = int(50)
+i = int(1)
+while i<=loopTis:
     msg = lst[int(time.time())%len(lst)]
+    i = i +1
+    #将测试消息复制到剪切板中
     w.OpenClipboard()
     w.EmptyClipboard()
     w.SetClipboardData(win32con.CF_UNICODETEXT, msg)
     w.CloseClipboard()
-    time.sleep(5)
+    time.sleep(flashTi)
 
     #填充消息
     win32gui.SendMessage(handle, 770, 0, 0)
